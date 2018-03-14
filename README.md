8158f4f476dab6d26f88aac2e1fef013fb06a756
Asuka Suzuki <hello@tanksuzuki.com>
2018-03-14 23:58:31 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # homebrew-jig
-brew formula for jig
+brew formula for [jig](https://github.com/tanksuzuki/jig)
---
+++ jig.rb
@@ -0,0 +1,9 @@
+class Jig < Formula
+  homepage "https://github.com/tanksuzuki/jig"
+  url "https://github.com/tanksuzuki/jig/releases/download/v0.0.2/jig_macosx_amd64"
+  version "0.0.2"
+  sha256 "379eabf0c04cefea3424539c50bbceab92fddfbbe0188c0ff4fe53d10a00f1ed"
+  def install
+    bin.install "jig"
+  end
+end
